60d9a54e79aa6af2f7413faffe835f7b75bfd1ad
shashank <shashankshekhar8534@gmail.com>
2024-06-01 20:43:35 +0530

    changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 
 const Counter=()=> {
-    const [Count,setCount] = useState([<></>])
+    const [Count,setCount] = useState(0)
 
     console.log(Count)
   
 
   return (
     <div>
-    <div>Counter - a{Count}</div>
-    <button onClick={()=>{setCount(Count+[<h1>hi</h1>])}}>increment</button>
+    <div>Counter - a{Count+1}</div>
+    <button onClick={()=>{setCount(Count+1)}}>increment</button>
     </div>
   )
 }
