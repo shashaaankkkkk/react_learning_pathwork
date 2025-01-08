02f440dc1c5a44ea460aff2b1435bb6ea9f89a12
shashank <shashankshekhar8534@gmail.com>
2025-01-08 10:47:02 +0530

    added changes to hello

--- src/components2/Hello.jsx
+++ src/components2/Hello.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hello(props) {
   return (
-    <div><h1>{props.llo} {props.second} heellp</h1></div>
+    <div><h1>{props.llo} {props.second}</h1></div>
   )
 }
 
